36d6deb280f2716d4cf47fa70c8827b03b56d7c6
Tyson <tysonshiv8@gmail.com>
2026-06-22 18:28:53 +0530

    feat: add central plaza, shops, and town environment props

--- frontend/src/Scenes/OutsideWorldScene.jsx
+++ frontend/src/Scenes/OutsideWorldScene.jsx
@@ -8,6 +8,15 @@ import Fountain from "../assets/Fountain.png";
 import Lamp from "../assets/Lamp.png";
 import Base from "../assets/Base.png";
 import Pavement from "../assets/Pavement.png";
+import Shop from "../assets/Shop.png";
+import CostumeStore from "../assets/CostumeStore.png"; 
+import FlowerPot2 from "../assets/FlowerPot2.png"
+import Bin from "../assets/Bin.png";
+import MartSign from "../assets/MartSign.png";
+import Statue from "../assets/Statue.png";
+import ClothSign from "../assets/clothSign.png";
+import Bench from "../assets/Bench.png";
+import FlowerPot3 from "../assets/FlowerPot3.png"
 
 class OutsideWorldScene extends Phaser.Scene{
     constructor(){
@@ -23,6 +32,16 @@ class OutsideWorldScene extends Phaser.Scene{
         this.load.image("Lamp",Lamp);
         this.load.image("Base",Base);
         this.load.image("Pavement",Pavement);
+        this.load.image("Shop",Shop);
+        this.load.image("CostumeStore",CostumeStore);
+        this.load.image("FlowerPot2",FlowerPot2);
+        this.load.image("Bin",Bin);
+        this.load.image("MartSign",MartSign);
+        this.load.image("Statue",Statue);
+        this.load.image("ClothSign",ClothSign);
+        this.load.image("Bench",Bench);
+        this.load.image("FlowerPot3",FlowerPot3);
+
     }
     create(){
         this.add.rectangle(0,0,3000,3000,0x3cb043).setOrigin(0);
@@ -35,17 +54,27 @@ class OutsideWorldScene extends Phaser.Scene{
                     .setDisplaySize(tileSize, tileSize);
             }
         }
-        this.Pavement = this.add.image(900,1100,"Pavement").setScale(1.2)
-        this.ArcadeBuilding = this.add.image(900,380,"ArcadeBuilding").setScale(0.5);
-        
-        this.leftPot = this.add.image(725,540,"FlowerPot").setScale(0.085);
-        this.righPot = this.add.image(1075,540,"FlowerPot").setScale(0.085);
-        const bounds =
-        this.ArcadeBuilding.getBounds();
-        this.buildingCollision = new Phaser.Geom.Rectangle(740,430,320,50);
-        this.leftPillarCollision =new Phaser.Geom.Rectangle(835,470,55,80);
-        this.rightPillarCollision =new Phaser.Geom.Rectangle(935,470,35,80);
-        this.entranceZone = new Phaser.Geom.Rectangle(850,500,85,40);
+        this.Pavement = this.add.image(1200,1100,"Pavement").setScale(1.2)
+        this.ArcadeBuilding = this.add.image(1200,380,"ArcadeBuilding").setScale(0.5);
+
+       this.shop = this.add.image(1850, 580, "Shop").setScale(0.37);
+        this.coustumeStore = this.add.image(550, 580, "CostumeStore").setScale(0.38);
+
+        this.FlowerPot2 = this.add.image(1960,700,"FlowerPot2").setScale(0.35);
+        this.Bin = this.add.image(2035,700,"Bin").setScale(0.13);
+        this.MartSign = this.add.image(1730,690,"MartSign").setScale(0.08);
+
+
+        this.Statue = this.add.image(450,705,"Statue").setScale(0.07);
+        this.ClothSign = this.add.image(650,705,"ClothSign").setScale(0.08);
+
+        this.leftPot = this.add.image(990,560,"FlowerPot").setScale(0.1);
+        this.righPot = this.add.image(1400,560,"FlowerPot").setScale(0.1);
+        const bounds = this.ArcadeBuilding.getBounds();
+        this.buildingCollision = new Phaser.Geom.Rectangle(1040,430,320,50);
+        this.leftPillarCollision =new Phaser.Geom.Rectangle(1135,470,55,80);
+        this.rightPillarCollision =new Phaser.Geom.Rectangle(1235,470,35,80);
+        this.entranceZone = new Phaser.Geom.Rectangle(1150,500,85,40);
 
         this.player = this.add.image(900,1150,"ghost").setScale(0.018);
         this.cameras.main.startFollow(this.player);
@@ -56,11 +85,18 @@ class OutsideWorldScene extends Phaser.Scene{
                 color: "#ffffff",
                 backgroundColor: "#000000"
             });
-        this.fountain = this.add.image(900,1050,"Fountain").setScale(0.3);
-        this.upperLeftLamp = this.add.image(700,800,"Lamp").setScale(0.15);
-        this.upperRightLamp = this.add.image(1098,800,"Lamp").setScale(0.15);
-        this.leftLamp = this.add.image(720,1300,"Lamp").setScale(0.15);
-        this.rightLamp = this.add.image(1070,1300,"Lamp").setScale(0.15);
+
+        this.rightFlowerPot = this.add.image(1670,900,"FlowerPot3").setScale(0.13);
+        this.leftFlowerPot = this.add.image(740,900,"FlowerPot3").setScale(0.13);
+
+        this.fountain = this.add.image(1200,1050,"Fountain").setScale(0.3);
+        this.upperLeftLamp = this.add.image(1000,800,"Lamp").setScale(0.15);
+        this.upperRightLamp = this.add.image(1398,800,"Lamp").setScale(0.15);
+        this.leftLamp = this.add.image(1020,1300,"Lamp").setScale(0.15);
+        this.rightLamp = this.add.image(1370,1300,"Lamp").setScale(0.15);
+        this.rightBench = this.add.image(1670,1075,"Bench").setScale(0.15);
+        this.leftBench = this.add.image(750,1075,"Bench").setScale(0.15);
+
         this.enterArcadeText.setVisible(false);
 
         this.keys = this.input.keyboard.addKeys({
